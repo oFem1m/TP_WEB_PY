634b06f63856e55752c9fd70e125937946d5c36d
alex <volohovaleksandr1974@gmail.com>
2023-11-08 22:34:58 +0300

    all files

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def question(request):
+    return render(request, 'question.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'register.html')
